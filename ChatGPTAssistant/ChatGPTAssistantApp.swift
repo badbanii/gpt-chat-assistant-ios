f68713f4a3d83e203079b2a5b536fae0471ab8fd
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-20 16:46:47 +0200

    Initial Commit

---
+++ ChatGPTAssistant/ChatGPTAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAssistantApp.swift
+//  ChatGPTAssistant
+//
+//  Created by Vlad on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
